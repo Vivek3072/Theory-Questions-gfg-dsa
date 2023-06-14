681fff866588dcc20ec90030e0955be1ecd52c3d
Vivek Srivastava <srivastavavivek012@gmail.com>
2023-06-15 00:26:14 +0530

    merge sort

---
+++ 7.Sorting/MergeSortFxn.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Merge(vector<int> &nums, int low, int mid, int high){
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+
+    vector<int> LNums(n1);
+    vector<int> RNums(n2);
+
+    for (int i = 0; i < n1; ++i)
+        LNums[i] = nums[low + i];
+    for (int j = 0; j < n2; ++j)
+        RNums[j] = nums[mid + 1 + j];
+
+    int i = 0, j = 0, k = low;
+
+    while (i < n1 && j < n2){
+        if (LNums[i] <= RNums[j]){
+            nums[k] = LNums[i];
+            i++;
+        }
+        else{
+            nums[k] = RNums[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1){
+        nums[k] = LNums[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2){
+        nums[k] = RNums[j];
+        j++;
+        k++;
+    }
+}
+
+void MergeSort(vector<int> &nums, int low, int high){
+    if (low < high){
+        int mid = low + (high - low) / 2;
+        MergeSort(nums, low, mid);
+        MergeSort(nums, mid + 1, high);
+        Merge(nums, low, mid, high);
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    MergeSort(nums, 0, n - 1);
+    for (int i = 0; i < n; i++)
+        cout << nums[i] << " ";
+    return 0;
+}
